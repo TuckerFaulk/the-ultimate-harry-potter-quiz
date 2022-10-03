16172c110bd38962302e208ae6702d0a32f7fbe4
TuckerFaulk <faulkner1993@hotmail.co.uk>
2022-10-03 14:34:29 +0000

    Add js file and pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+/*
+
+object {
+Character List: []
+Remaining: 156,
+Score: 0
+}
+
+2. Display "Character to Select" from Character List.
+3. Event listeners to prev and next buttons to toggle through the characters.
+    - .addClass("invisible to "prev" when Character List[0])
+
+4. Fuction to updateRemaining - Minus 1 from remaining.
+
+5. Function to updateScore - Add 1 to the score.
+
+6. Add event listeners to all images
+
+7. if (image clicked alt === character to select) {
+    .removeClass("invisible").addClass("correct")
+    remove alt selected from character_list
+    updateScore();
+    updateRemaining();
+} else {
+    .removeClass("invisible").addClass("incorrect")
+    remove alt selected from character_list
+    updateRemaining();
+}
+
+8. gameEnd display: Display results
+9. Seperate for 100% correct
+*/
